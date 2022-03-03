70c15ac887df81d024ba65fe59f86f0de2ba7408
Joseph Wilson <jgwilsondesign@gmail.com>
2022-03-02 20:40:47 -0800

    initial commit

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<button on:click>
+	<slot />
+</button>
+
+<style lang="scss" scoped>
+	button {
+		background: tomato;
+		border: 0;
+		border-radius: 0.2rem;
+		box-sizing: border-box;
+		color: white;
+		cursor: pointer;
+		font: normal 600 1rem/2rem sans-serif;
+		padding: 0.2rem 0.8rem;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+	let count = 0;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+	Count: <strong>{count}</strong>
+	<Button on:click={() => count++}>Increment Counter</Button>
+</p>
